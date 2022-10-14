a3aa1b1631c8d0d6a4c3fcaf379464ce1f0dd114
Ramon Oliveira <ramon.ricardo13@gmail.com>
2022-10-14 14:39:23 -0300

    footer arrumado e js atualizado

--- pagina-formacao/app.js
+++ pagina-formacao/app.js
@@ -1,6 +1,10 @@
 let hamburguerMenu = document.getElementsByClassName("mobile-menu")[0]
 function showMenu() {
-    let navList = document.getElementsByClassName("nav-list")[0];
+    let navList = document.getElementsByClassName('nav-list')[0];
+    let iconUser = document.getElementById('icon');
+    if(iconUser) {
+        iconUser.parentNode.removeChild(iconUser);
+    }
     navList.classList.toggle('active');
     hamburguerMenu.classList.toggle('active');
 }
